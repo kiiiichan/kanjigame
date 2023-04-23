180c10600d190bb25562b8b837674c9a3521c8b9
hashi.h <hashi.h@vpj.co.jp>
2023-04-23 13:04:53 +0900

    画像が正しく表示されない不具合の解消

--- src/QuestionDisplay.tsx
+++ src/QuestionDisplay.tsx
@@ -1,29 +1,44 @@
-import React from 'react';
-import AccessibilityNewIcon from '@mui/icons-material/AccessibilityNew';
-import { Timer } from './Timer';
-import { Box } from '@mui/material';
+import React from "react";
+import { Timer } from "./Timer";
+import { Box } from "@mui/material";
 
 type QuestionDisplayProps = {
   question: string;
   startSec: number;
-}
+};
 
-export const QuestionDisplay: React.VFC<QuestionDisplayProps> = ({question,startSec}) => {
-  
+export const QuestionDisplay: React.VFC<QuestionDisplayProps> = ({
+  question,
+  startSec,
+}) => {
   return (
     <Box
-    sx={{ height:"2000px", width: "100%", backgroundImage: "url('/assets/images/panda.png')" }}
+      sx={{
+        width: "100%",
+      }}
     >
-    <div style={{height:"100vh", backgroundImage: "url('/assets/images/panda.png')"}}>
-      <div style={{display:"flex",justifyContent:"flex-end"}}>
-        <Timer startSec={startSec}></Timer>
+      <div
+        style={{
+          height: "80vh",
+          backgroundImage: "url('/assets/images/panda.png')",
+          backgroundSize: "contain",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+        }}
+      >
+        <div style={{ display: "flex", justifyContent: "flex-end" }}>
+          <Timer startSec={startSec}></Timer>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <h1>{question}</h1>
+        </div>
       </div>
-      <div style={{display:"flex",justifyContent:"center",alignItems:"center"}}>
-        <h1>
-          {question}
-        </h1>
-      </div>
-    </div>
     </Box>
   );
 };
